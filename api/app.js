7c2793e5eb2dc2ab98b089e114460b11661cb0fa
Aleksandr Khokhlachev <demcy.meizu@gmail.com>
2021-02-17 09:03:42 +0200

    http

--- api/app.js
+++ api/app.js
@@ -174,3 +174,5 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+module.exports = http
